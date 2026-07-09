7f88b20e62d1e72e25133738f123065896e58041
Joshtr08083 <joshtreid08@gmail.com>
2026-07-09 02:52:21 -0400

    added notes/warnings/callouts

--- app/components/PageModules/ModuleRenderer.tsx
+++ app/components/PageModules/ModuleRenderer.tsx
@@ -3,13 +3,16 @@ import { TextProps } from "./TextModule"
 import { DividerProps } from "./DividerModule"
 import { CodeProps } from "./CodeModule"
 import { HeaderProps } from "./HeaderModule"
+import { NoteProps  } from "./NoteModule"
 import ImageModule from "./ImageModule"
 import TextModule from "./TextModule"
 import DividerModule from "./DividerModule"
 import CodeModule from "./CodeModule"
 import HeaderModule from "./HeaderModule"
+import NoteModule from "./NoteModule"
+import { Header } from "next/dist/lib/load-custom-routes"
 
-export type Module = ImageProps | TextProps | DividerProps | CodeProps
+export type Module = ImageProps | TextProps | DividerProps | CodeProps | HeaderProps | NoteProps
 
 interface Props {
     module: Module
@@ -22,6 +25,7 @@ const ModuleRenderer = ({module} : Props) => {
     case "divider": return <DividerModule {...module as DividerProps} />
     case "code": return <CodeModule {...module as CodeProps} />
     case "header": return <HeaderModule {...module as HeaderProps} />
+    case "note": return <NoteModule {...module as NoteProps} />
     default: return null
   }
 }
--- app/projects/[id]/layout.tsx
+++ app/projects/[id]/layout.tsx
@@ -4,7 +4,7 @@ import BackButton from "@/app/components/Buttons/BackButton";
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <div className="flex w-full flex-col justify-start relative">
-        <BackButton nav classes={"absolute shadow-xl/50"} url="/view-projects"/>
+        <nav><BackButton nav classes={"absolute shadow-xl/50"} url="/view-projects"/></nav>
         {children}
     </div>
   );
