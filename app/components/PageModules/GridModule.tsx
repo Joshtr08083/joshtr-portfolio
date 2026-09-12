c356da09e0a6de3b62c6de7555c8141cbf6a9eec
HengTao2 <awesometao08@gmail.com>
2026-09-12 19:36:27 -0400

    Added align and justify to grid

--- app/components/PageModules/GridModule.tsx
+++ app/components/PageModules/GridModule.tsx
@@ -2,19 +2,20 @@ import { GridProps } from "./types.d"
 import styles from "./PageModules.module.css"
 import positionStyles from "./PositionStyles"
 
-const SectionModule = ({gap=10, rounded=true, columns=2, dynamic=true, backgroundColor="var(--panel-color)", showBack=false, children, positioning=undefined} : GridProps) => {
+const SectionModule = ({gap=10, rounded=true, columns=2, dynamic=true, backgroundColor="var(--panel-color)", showBack=false, children, positioning=undefined, align="center", justify="center", dynamicMinCellWidth=250} : GridProps) => {
   return (
     <div 
         className={`
-            w-full grid items-center justify-center
+            w-full grid
             ${rounded? "rounded-2xl" : ""}
-            ${dynamic? styles.dynamicGridContainer : ''}
         `}
         style={{
             ...positionStyles({...positioning}),
             backgroundColor: (showBack)? backgroundColor : "transparent",
-            gridTemplateColumns: `${dynamic? undefined : `repeat(${columns}, minmax(0, 1fr))`}`,
-            gap: `calc(var(--spacing) * ${gap})`
+            gridTemplateColumns: `${dynamic? `repeat(auto-fit, minmax(${dynamicMinCellWidth}px, 1fr))`: `repeat(${columns}, minmax(0, 1fr))`}`,
+            gap: `calc(var(--spacing) * ${gap})`,
+            alignItems: align,
+            justifyItems: justify
         }}    
     >
         {children}
