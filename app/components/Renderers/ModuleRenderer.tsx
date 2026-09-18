45ebd14caaeac7cd3349be4942809a9d8c6a42bc
Joshtr08083 <joshtreid08@gmail.com>
2026-09-17 22:00:27 -0400

    Fixed ModuleRendere switch-case scope issue

--- app/components/Renderers/ModuleRenderer.tsx
+++ app/components/Renderers/ModuleRenderer.tsx
@@ -14,35 +14,46 @@ import GridModule from "@/app/components/PageModules/GridModule"
 import ContainerModule from "@/app/components/PageModules/ContainerModule"
 import YoutubeModue from "@/app/components/PageModules/YoutubeModule"
 
-export type Module = (Types.ImageProps | Types.TextProps | Types.DividerProps | Types.CodeProps | Types.HeaderProps | Types.NoteProps |  Types.ListProps | Types.SectionProps | Types.YoutubeProps)
+export type Module = (Types.ImageProps | Types.TextProps | Types.DividerProps | Types.CodeProps | Types.HeaderProps | Types.NoteProps |  Types.ListProps | Types.SectionModuleProps | Types.YoutubeProps | Types.ContainerModuleProps | Types.GridModuleProps)
 
 const getModule = ( module: Module, key:number,) => {
     switch (module.type) {
-        case "image": 
+        case "image": {
             return <ImageModule key={key} {...module as Types.ImageProps}/>
-        case "text":
+        }
+        case "text": {
             return <TextModule key={key} {...module as Types.TextProps}/>
-        case "divider": 
+        }
+        case "divider": {
             return <DividerModule key={key} {...module as Types.DividerProps} />
-        case "code": 
+        }
+        case "code": {
             return <CodeModule key={key} {...module as Types.CodeProps} />
-        case "header": 
+        }
+        case "header": {
             return <HeaderModule key={key} {...module as Types.HeaderProps} />
-        case "note": 
+        }
+        case "note": {
             return <NoteModule key={key} {...module as Types.NoteProps} />
-        case "list":
+        }
+        case "list": {
             return <ListModule key={key} {...module as Types.ListProps} />
-        case "ytvideo":
+        }
+        case "ytvideo": {
             return <YoutubeModue key={key} {...module as Types.YoutubeProps} />
-        case "section":
-            const {elements: section_data, ...section_props} = module as Types.SectionModuleProps;
-            return <SectionModule key={key} {...section_props}>{renderModules(section_data)}</SectionModule>
-        case "grid":
-            const {elements: grid_data, ...grid_props} = module as Types.GridModuleProps;
-            return <GridModule key={key} {...grid_props}>{renderModules(grid_data)}</GridModule>
-        case "container":
-            const {elements: container_data, ...container_props} = module as Types.ContainerModuleProps;
-            return <ContainerModule key={key} {...container_props}>{renderModules(container_data)}</ContainerModule>
+        }
+        case "section": {
+            const {elements: data, ...props} = module as Types.SectionModuleProps;
+            return <SectionModule key={key} {...props}>{renderModules(data)}</SectionModule>
+        }
+        case "grid": {
+            const {elements: data, ...props} = module as Types.GridModuleProps;
+            return <GridModule key={key} {...props}>{renderModules(data)}</GridModule>
+        }
+        case "container": {
+            const {elements: data, ...props} = module as Types.ContainerModuleProps;
+            return <ContainerModule key={key} {...props}>{renderModules(data)}</ContainerModule>
+        }
         default: 
             return null
     }
